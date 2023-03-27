7ef37d2f5528edade80f7c6e744788f81249d356
Hasib-Ahamed <hasibahamedsakib1@gamil.com>
2023-03-27 23:25:25 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../../images/Logo.svg";
+import "../Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <nav className="nav-bar">
+        <img src={logo} alt="image" />
+
+        <div className="nav-link">
+          <a href="/order">Order</a>
+          <a href="/order-review">Order Review</a>
+          <a href="/manage-inventory">Manage Inventory</a>
+          <a href="/login">LogIn</a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
